d1d3496ed1c200d65994a1739ba987c94abac185
alailsonribeiro <ribeiro@alailson.com.br>
2020-03-28 13:22:36 -0300

    Base app in openode.io

--- app.js
+++
@@ -1,7 +0,0 @@
-/**
- * @ Author: Aailson Barbosa Ribeiro
- * @ Create Time: 2020-03-28 05:15:27
- * @ Modified by: ribeiro@alailson.com.br
- * @ More info: http://alailson.com.br
- * @ Modified time: 2020-03-28 05:16:12
- */
