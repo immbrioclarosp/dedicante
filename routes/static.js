aa30881ece70bba210ca21958c9bd7cea63bc855
alailsonribeiro <ribeiro@alailson.com.br>
2020-04-06 19:30:41 -0300

    Acerto Tela de solicitação de registro Validação de campos

--- routes/static.js
+++ routes/static.js
@@ -13,8 +13,22 @@ module.exports.static = (app) => {
 
     app.use('/www', express.static('www'));
     console.log("/www");
-    app.use('/static', express.static('static'));
+    app.use('/static', express.static('www/static'));
     console.log("/static");
+    app.use('/service-worker.js', express.static('www/service-worker.js'));
+    console.log("/service-worker.js");
+    app.use('/service-worker.js.map', express.static('www/service-worker.js.map'));
+    console.log("/service-worker.js.map");
+    app.use('/manifest.json', express.static('www/manifest.json'));
+    console.log("/manifest.json");
+    app.use('/index.html', express.static('www/index.html'));
+    console.log("/index.html");
+    app.use('/fonts', express.static('www/fonts'));
+    console.log("/fonts");
+    app.use('/css', express.static('www/css'));
+    console.log("/css");
+    app.use('/js', express.static('www/js'));
+    console.log("/js");
     app.use('/bootstrap', express.static('node_modules/bootstrap'));
     console.log("/bootstrap");
 
